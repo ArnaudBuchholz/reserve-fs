34444f935470c74d8ed1a359e75920485ca06a49
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-01-17 13:26:26 -0500

    Enables coverage info (still failing)

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-(function () {
+module.exports = function () {
   'use strict'
 
   var APIs = ''
@@ -6,6 +6,63 @@
   var URL = ''
 
   var fs = {}
+
+  function buildWrapper (members) {
+    var Wrapper = function (values) {
+      Object.keys(values)
+        .filter(function (property) {
+          return property !== '$class'
+        })
+        .forEach(function (property) {
+          this[property] = values[property]
+        }, this)
+    }
+    members.forEach(function (name) {
+      Wrapper.prototype[name] = function () {
+        return this['_' + name]
+      }
+    })
+    return Wrapper
+  }
+
+  var direntMethods = 'isBlockDevice,isCharacterDevice,isDirectory,isFIFO,isFile,isSocket,isSymbolicLink'.split(',')
+  var statDatePrefixes = 'a,m,c,birth'.split(',')
+
+  var unwrappers = {}
+  unwrappers.Dirent = buildWrapper(direntMethods)
+  unwrappers.Stat = function () {
+    unwrappers.Dirent.apply(this, arguments)
+    statDatePrefixes.forEach(function (prefix) {
+      this[prefix + 'time'] = new Date(this[prefix + 'timeMs'])
+    }, this)
+  }
+  unwrappers.Stat.prototype = Object.create(unwrappers.Dirent.prototype)
+
+  function unwrap (result) {
+    if (result.$class) {
+      return new unwrappers[result.$class](result)
+    }
+    return result
+  }
+
+  function parseResponse (responseText) {
+    try {
+      var response = JSON.parse(responseText)
+      if (response.err) {
+        return response
+      }
+      if (Array.isArray(response.result)) {
+        response.result = response.result.map(unwrap)
+      } else {
+        response.result = unwrap(response.result)
+      }
+      return response
+    } catch (e) {
+      return {
+        err: e
+      }
+    }
+  }
 
   function api (name) {
     return function () {
@@ -16,7 +73,7 @@
       xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
           if (xhr.status === 200) {
-            var response = JSON.parse(xhr.responseText)
+            var response = parseResponse(xhr.responseText)
             if (response.err) {
               callback(new Error(response.err))
             } else {
@@ -62,4 +119,4 @@
   })
 
   window[NAME] = fs
-}())
+}
