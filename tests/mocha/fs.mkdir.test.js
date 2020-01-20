0ecb02c3554d13c796a2e08d762cc4d11570f015
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-01-20 12:59:23 -0500

    fs.mkdir test

---
+++ tests/mocha/fs.mkdir.test.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const assert = require('./assert')
+
+describe('fs.mkdir', async () => {
+  before(() => require('./setup'))
+  after(() => window.fsw.rmdirAsync('tmp/a/b/c')
+    .then(() => window.fsw.rmdirAsync('tmp/a/b'))
+    .then(() => window.fsw.rmdirAsync('tmp/a'))
+    .then(() => window.fsw.rmdirAsync('tmp'))
+  )
+
+  let method
+
+  before(() => {
+    method = window.fsw.mkdirAsync.bind(window.fsw)
+  })
+
+  it('allows folder creation', () => method('tmp')
+    .then(() => window.fsw.statAsync('tmp'))
+    .then(stat => assert(() => stat.isDirectory()))
+  )
+
+  it('allows recursive folder creation', () => method('tmp/a/b/c', { recursive: true })
+    .then(() => window.fsw.statAsync('tmp/a/b'))
+    .then(stat => assert(() => stat.isDirectory()))
+    .then(() => window.fsw.statAsync('tmp/a/b/c'))
+    .then(stat => assert(() => stat.isDirectory()))
+  )
+
+  it('forward errors (folder already exists)', () => method('tmp/a/b')
+    .then(assert.notExpected, reason => {
+      // console.log(reason.toString())
+      assert(() => reason instanceof Error)
+    })
+  )
+
+  it('forbids unauthorized access', () => method('../mocha/tmp')
+    .then(assert.notExpected, reason => {
+      // console.log(reason.toString())
+      assert(() => reason instanceof Error)
+    })
+  )
+})
