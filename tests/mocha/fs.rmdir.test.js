947541d9a4f7fff207ff4ee65fadbc2b89104318
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-01-20 12:59:39 -0500

    fs.rmdir test

---
+++ tests/mocha/fs.rmdir.test.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const assert = require('./assert')
+
+describe('fs.rmdir', async () => {
+  before(() => require('./setup'))
+  after(() => window.fsw.rmdirAsync('tmp'))
+
+  let method
+
+  before(() => {
+    method = window.fsw.rmdirAsync.bind(window.fsw)
+  })
+
+  before(() => window.fsw.mkdirAsync('tmp/a', { recursive: true }))
+
+  it('allows folder deletion', () => window.fsw.statAsync('tmp/a')
+    .then(stat => assert(() => stat.isDirectory()))
+    .then(() => method('tmp/a'))
+    .then(() => window.fsw.statAsync('tmp/a'))
+    .then(assert.notExpected, reason => {
+      assert(() => reason instanceof Error)
+    })
+    .then(() => window.fsw.statAsync('tmp'))
+    .then(stat => assert(() => stat.isDirectory()))
+  )
+
+  it('forward errors (folder does not exists)', () => method('tmp/b')
+    .then(assert.notExpected, reason => {
+      // console.log(reason.toString())
+      assert(() => reason instanceof Error)
+    })
+  )
+
+  it('forbids unauthorized access', () => method('../mocha')
+    .then(assert.notExpected, reason => {
+      // console.log(reason.toString())
+      assert(() => reason instanceof Error)
+    })
+  )
+})
